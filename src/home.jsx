a4a2eca9fb126ae483f00b721799a8628ea4af2c
bennyd6 <228r1a66d6@cmrec.ac.in>
2024-08-23 22:49:02 +0530

    heading updated

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,7 @@
 function Home(){
     return(
         <div className="home-bg">
-            <h1 className="main-heading">Welcome, I'm Benny</h1>
+            <h1 className="main-heading">Welcome,<br /> I'm <span> Benny</span></h1>
         </div>
     )
 }
