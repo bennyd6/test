4a6d4a0691fbeff3b875fde8d7a25a96ac6e6ff6
bennyd6 <228r1a66d6@cmrec.ac.in>
2024-08-23 23:37:10 +0530

    navbar updated

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,13 @@
+import Navbar from "./navbar";
 function Home(){
     return(
         <div className="home-bg">
-            <h1 className="main-heading">Welcome,<br /> I'm <span> Benny</span></h1>
+            <div className="home-up">
+                <Navbar></Navbar>
+            </div>
+            <div className="home-down">
+                <h1 className="main-heading">Welcome,<br /> I'm <span> Benny</span></h1>
+            </div>
         </div>
     )
 }
---
+++ src/navbar.jsx
@@ -0,0 +1,13 @@
+function navbar(){
+    return(
+        <div id="navbar">
+            <a href="#main-ctn" class="sections">Home</a>
+            <a class="sections" href="#About">About</a>
+            <a class="sections" href="#skills">Skills</a>
+            <a class="sections" href="#projects">Projects</a>
+            <a class="sections" href="#contact">Contact</a>
+        </div>
+    )
+}
+
+export default navbar;
