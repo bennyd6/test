61d76cdd4a9f0c65d5f86ae4988d74b45333c532
bennyd6 <228r1a66d6@cmrec.ac.in>
2024-09-14 10:54:43 +0530

    project updated

--- src/projects.jsx
+++ src/projects.jsx
@@ -2,6 +2,8 @@ import Card from "./card"
 import img from './assets/blob-haikei.png'
 import {Fade} from "react-awesome-reveal"
 import Gen from './assets/genesis.png'
+import Cre from './assets/crexo.png'
+import Num from './assets/numbers.png'
 function Projects(){
     return(
         <section className='projects-main' id="projects">
@@ -16,10 +18,10 @@ function Projects(){
                 <Card heading='Genesis' a='https://github.com/bennyd6/genesis' img={Gen} para="Genesis is a comprehensive agricultural technology platform integrating crop prediction, analysis, and drone/rover controls for enhanced farming efficiency."></Card>
                 </Fade>
                 <Fade direction='right' delay={400}>
-                <Card heading='Crexo' a='https://github.com/bennyd6/crexo' img={img}  para="Crexo is a creative platform for storytelling, poetry, and other artistic content, offering a space for creators to share and explore diverse works."></Card>
+                <Card heading='Crexo' a='https://github.com/bennyd6/crexo' img={Cre}  para="Crexo is a creative platform for storytelling, poetry, and other artistic content, offering a space for creators to share and explore diverse works."></Card>
                 </Fade>
                 <Fade direction='right' delay={600}>
-                <Card heading='Crexo' a='https://github.com/bennyd6/genesis' img={img} para="Genesis is a comprehensive agricultural technology platform integrating crop prediction, analysis, and drone/rover controls for enhanced farming efficiency."></Card>
+                <Card heading='Numbers' a='https://github.com/bennyd6/' img={Num} para="Numbers is an innovative project aimed at creating a digital twin of a farm. This virtual model replicates the physical environment of a farm, integrating real-time data from various sensors."></Card>
                 </Fade>
             </div>
         </section>
